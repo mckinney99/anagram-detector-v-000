c86cccaadc6376b65a96059328c69702c538ced8
Eric McKinney <mckinney99@gmail.com>
2017-11-06 05:50:43 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word 
+
+  def initialize
+    @word = word
+  end
+
+  def match
+    @word = word.each do |word|
+      word.
+  end
+
+  
+return %w word
